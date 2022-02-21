71022b4329c02d3be911c1ca107bc83a45e0a816
Dente Luciano <lucianodente152@gmail.com>
2022-02-21 00:31:58 -0300

    Auto response

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
 
-
 const SESSION_FILE_PATH = "./session.js";
 const COUNTRY_CODE = "549";
 const NUMBER_PHONE = "3764721581";
@@ -15,8 +14,6 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
 
 const client = new Client({ session: session_data });
 
-client.initialize();
-
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
 });
@@ -27,7 +24,7 @@ client.on('ready', () => {
     let chatId = COUNTRY_CODE + NUMBER_PHONE + "@c.us";
     client.sendMessage(chatId, MSG)
         .then((response) => {
-            if(response.id.fromMe) console.log("Mensaje enviado.");
+            if (response.id.fromMe) console.log("Mensaje enviado.");
         });
 
 });
@@ -35,11 +32,19 @@ client.on('ready', () => {
 client.on('authenticated', (session) => {
     session_data = session;
     fs.writeFileSync(
-        SESSION_FILE_PATH, //Ubicacion del archivo
-        JSON.stringify(session), //En caso de success
-        (error) => { if (error) console.error(error) }); //En caso de error
+        SESSION_FILE_PATH,
+        JSON.stringify(session),
+        (error) => { if (error) console.error(error) }
+    );
 });
 
 client.on('auth_failure', (msg) => {
     console.error("Hubo un fallo en la autenticación: ", msg);
 });
+
+client.on('message', async msg => { //Para cuando recibe un msj
+    console.log('MESSAGE RECEIVED', msg);
+    client.sendMessage(msg.from, 'Hola automatico!');
+});
+
+client.initialize();
